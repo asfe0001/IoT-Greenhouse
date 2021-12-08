f1b69f1426f73303d076b0df259b0d9f94bf2daf
Claudio O <56867330+kruemelbacke@users.noreply.github.com>
2021-12-08 17:23:30 +0100

    fix: fixed value

--- group01_air/code_snippets/mqtt_test.py
+++ group01_air/code_snippets/mqtt_test.py
@@ -1,15 +1,30 @@
 import paho.mqtt.client as mqtt
 import json
+import time
  
 username = "ubuntu"
 password = "ubuntu"
 adress = "cf8da10a-0d29-41a9-9c59-beb2051be054.ka.bw-cloud-instance.org"
 
-message = json.dumps({"testvalue" : "99"})
+
 
 client = mqtt.Client()
 client.username_pw_set(username, password)
 client.connect(adress, 1883, 60)
- 
-client.publish("Greenhouse/Air/Nachrichtanmarvin", message)
+i=0
+while (True):
+    message = json.dumps({"testvalue" : i})
+    time.sleep(1)
+
+    value = i
+
+    message = json.dumps({"Temperature" : value})
+                            
+
+
+    client.publish("Greenhouse/Air", message)
+    print("Sent message:"+str(message))
+
+    i = i+1
+
 client.disconnect()
