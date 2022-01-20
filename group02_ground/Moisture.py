119b463b54fd359da6a19aea75b219f950f2e451
3lack3lue <94557336+3lack3lue@users.noreply.github.com>
2022-01-20 01:26:36 +0100

    Use moisture sensor

--- group02_ground/Moisture.py
+++ group02_ground/Moisture.py
@@ -1,6 +1,30 @@
-def setupMoisture():
-    print('Moisture sensor setup done')
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
 
 def getMoisture():
-    moisture = 18
-    return moisture
+        try:
+                # Create the I2C bus
+                i2c = busio.I2C(board.SCL, board.SDA)
+
+                # Create the ADC object using the I2C bus
+                ads = ADS.ADS1015(i2c)
+
+                # Create single-ended input on channel 0
+                chan = AnalogIn(ads, ADS.P0)
+
+                moisture_min = 144                      # 0.018 V = 144 : 0%
+                moisture_max = 13584                    # 1.7V = 13584 : 100%
+
+                moisture_raw = chan.value
+
+                if moisture_raw <= moisture_min:
+                        moisture_raw = moisture_min
+                elif moisture_raw >= moisture_max:
+                        moisture_raw = moisture_max
+                moisture_percent = (100/(moisture_max - moisture_min)) * (moisture_raw - moisture_min)
+
+                return moisture_percent
+        except:
+                return float('nan')
