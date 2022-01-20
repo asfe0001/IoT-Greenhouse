ad6310fbeda945a15b07abbcae5b9fe542d126bf
3lack3lue <94557336+3lack3lue@users.noreply.github.com>
2022-01-20 01:27:12 +0100

    Use temperature sensor

--- group02_ground/Temperature.py
+++ group02_ground/Temperature.py
@@ -1,6 +1,10 @@
-def setupTemperature():
-    print('Temperture sensor setup done')
-
 def getTemperature():
-    temperature = 18
-    return temperature
+    try:
+        with open("/sys/bus/w1/devices/28-3c01e0769645/w1_slave") as tempfile:
+            inhalt = tempfile.read()
+            tempfile.close()
+        tempdata = inhalt.split("\n")[1].split(" ")[9]
+        temperatur = float(tempdata[2:])/1000
+        return temperatur
+    except:
+        return float('nan')
